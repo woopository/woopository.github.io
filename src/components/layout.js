7f589f828e6ab615dce955528a89cd1233f84231
withcoffee <tnsdn94560@gmail.com>
2022-12-19 23:23:53 +0900

    update: header, footer 레이아웃 분리

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,30 +2,18 @@ import * as React from "react"
 import { Link } from "gatsby"
 import ThemeToggle from "../components/ThemeToggle"
 
-const Layout = ({ location, title, children }) => {
-  const rootPath = `${__PATH_PREFIX__}/`
-  const isRootPath = location.pathname === rootPath
-  let header
-
-  if (isRootPath) {
-    header = (
-      <h1 className="main-heading h2">
-        <Link to="/">{title}</Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
-    )
-  }
-
+const Layout = ({ title, children }) => {
   return (
-    <div className="global-wrapper" data-is-root-path={isRootPath}>    
-      <header className="global-header">{header}</header>
-      <ThemeToggle />
-      <main>{children}</main>
+    <div className="global-wrapper">
+      <header className="global-header">
+        <h1 className="main-heading h2">
+          <Link to="/">{title}</Link>
+        </h1>
+        <ThemeToggle />
+      </header>
+      <div className="global-content">
+        <main>{children}</main>        
+      </div>
       <footer>
         © {new Date().getFullYear()}, Built with
         {` `}
